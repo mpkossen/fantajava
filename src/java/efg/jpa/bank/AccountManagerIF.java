7acc0cd6a3729ac41bde16f8c25d28ea7bd539d5
arno.becker <arno.becker@baad855c-c507-11dd-874b-8bacd04a0a74>
2009-01-20 10:57:56 +0000

    lol library

--- src/java/efg/jpa/bank/AccountManagerIF.java
+++
@@ -1,9 +0,0 @@
-package efg.jpa.bank;
-
-public interface AccountManagerIF
-{
-   String[] getAccount(String number);
-   String[][] getTransactions(String number);
-   String newAccount(double newLimit, String newName, String newPincode) throws BankException;
-   String setOpen(boolean b);
-}
--- src/java/efg/web/beans/NieuweAccount.java
+++ src/java/efg/web/beans/NieuweAccount.java
@@ -6,6 +6,9 @@
 package efg.web.beans;
 
 import efg.bank.AccountManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,9 +20,25 @@ public class NieuweAccount {
     
     public NieuweAccount(AccountManager am) {
 	accMan = am;
+	if (accMan == null) {
+	    String username = ""+""+100000;
+	    String password = "geheim";
+	    String salt = "" + System.currentTimeMillis();
+	    try {
+		accMan = new AccountManager(username, efg.bank.util.MD5.encode(efg.bank.util.MD5.hash(password+salt)),salt);
+	    }
+	    catch(Exception e) {
+		System.out.println(e);
+	    }
+	}
     }
     
-    public boolean voegNieuweAccountToe() {
+    public boolean voegNieuweAccountToe(Double limit, String name, String pincode ) {
+	try {
+	    accMan.newAccount(limit, name, pincode);
+	} catch (Exception ex) {
+	    Logger.getLogger(NieuweAccount.class.getName()).log(Level.SEVERE, null, ex);
+	} 
 	return false;
     }
 	
