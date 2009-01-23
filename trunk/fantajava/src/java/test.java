38222543041fca513cb356575e2ca24585af2a54
arno.becker <arno.becker@baad855c-c507-11dd-874b-8bacd04a0a74>
2009-01-23 15:41:39 +0000

    FUCK YEAH, JAAS WERKT :D.

---
+++ trunk/fantajava/src/java/test.java
@@ -0,0 +1,12 @@
+import efg.jpa.bank.*;
+import efg.jpa.bank.db.*;
+
+public class test {
+
+    public static void main(String[] args) {
+	//Main m = new Main();
+	//m.main(new String[]{"create"});
+	Database.createDatabase();
+	}
+    }
+
