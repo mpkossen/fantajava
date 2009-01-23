30ec2f15a4cf9178bcb255720f61cba8c1ab00ec
arno.becker <arno.becker@baad855c-c507-11dd-874b-8bacd04a0a74>
2009-01-23 22:22:58 +0000

    login-config.xml word nu correct in de war gepropt, dus beans zijn bereikbaar :D.

--- fantajava/src/java/beans/BankBean.java
+++ fantajava/src/java/beans/BankBean.java
@@ -10,17 +10,20 @@ package beans;
  * @author Bami
  */
 
-import java.security.Principal;
 
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 /* Deze bean regelt de login shit. De Bank.jsp vraagt aan deze bean welke subview hij moet presenten.
  * 
  * */
-public class BankBean
+public class BankBean extends CommonBean
 {      
         private ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
        
+	public BankBean() {
+	    System.out.println("BankBean();");
+	}
+	
         public boolean getDisplayManager()
         {
 		System.out.println("BankBean.getDisplayManager()");
