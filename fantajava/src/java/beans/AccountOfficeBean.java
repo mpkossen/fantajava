4730b298b8274d9549419edecaa162ab1efd7cd8
maarten.kossen <maarten.kossen@baad855c-c507-11dd-874b-8bacd04a0a74>
2009-01-23 09:26:43 +0000

    Troep.

---
+++ fantajava/src/java/beans/AccountOfficeBean.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package beans;
+
+import efg.jpa.bank.AccountOffice;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author mistermartin75
+ */
+public class AccountOfficeBean extends CommonBean
+{
+	private String id;
+	private AccountOffice accountOffice;
+	private static int ID = 0;
+	private HttpSession session = null;
+
+	private static int getId ()
+	{
+		return ID++;
+	}
+
+	public AccountOfficeBean()
+	{
+		session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+		if ( session != null )
+		{
+			setException("Id=" + id + ", sessionId=" + session.getId());
+			accountOffice = (AccountOffice) session.getAttribute("accountOffice");
+			if ( accountOffice != null )
+			{
+				return;
+			}
+		}
+		try
+		{
+			FacesContext.getCurrentInstance().getExternalContext().redirect("AccountOffice.faces");
+		}
+		catch ( Exception e )
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+
+/**
+ * Geeft alle transacties van de rekening number.
+ * @param number	het nummer van de account
+ * @return	een String[][]. De eerste array bevat nummers vanaf 0 tot en met het
+ * aantal van transacties. De tweede array bevat de informatie van een
+ * transactie (id, from, to, amount, transactiontime, transfertime)
+ */
+	public String[][] getTransactions (String number)
+	{
+		String[][] ret = accountOffice.getPendingTransacties();
+		return ret;
+	}
+}
---
+++ fantajava/web/fragments/oversight-transactions.jsp
@@ -0,0 +1,30 @@
+<%--
+    Document   : TransactieOverzicht
+    Created on : 19-Jan-2009, 13:38:48
+    Author     : mistermartin75
+--%>
+<f:subview>
+    <h:form>
+        <h:dataTable bgcolor="#F1F1F1" border="10" cellpadding="5" cellspacing="3" dir="LTR" first="0" frame="hsides" id="dt1" rows="4"
+            rules="all" summary="This is a JSF code to create dataTable." value="#{AccountOffice.transacties}" var="item" width="50%">
+            <f:facet name="header">
+                <h:outputText value="This is 'dataTable' demo"/>
+            </f:facet>
+            <h:column>
+                <f:facet name="header">
+                    <h:outputText value="soort"/>
+                </f:facet>
+            </h:column>
+            <h:column>
+                <f:facet name="header">
+                    <h:outputText value="name"/>
+                </f:facet>
+            </h:column>
+            <f:facet name="footer">
+                <h:outputText value="The End"/>
+            </f:facet>
+        </h:dataTable>
+        <br/>
+        <br/>
+    </h:form>
+</f:subview>
